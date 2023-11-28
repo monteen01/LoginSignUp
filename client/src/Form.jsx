4a629f3fea8c8ab92b916efdc56e4b8f7dd1d140
monteen <montwork01@gmail.com>
2023-11-28 11:27:23 +0530

    added form changes

--- client/src/Form.jsx
+++ client/src/Form.jsx
@@ -3,15 +3,21 @@ import { Link } from "react-router-dom";
 const Form = () => {
   return (
     <div className="container grid place-items-center mt-[10rem] ">
-      <form className="w-[700px] text-center ">
+      <form className="w-[300px] text-center border border-blue-500 rounded-md shadow-sm py-5  ">
         <h1 className="text-xl mb-2">Login or SignUp</h1>
         <div className="flex justify-evenly items-center">
-          <button className="mt-2 h-12 text-center px-6 py-2 shadow-sm rounded-md bg-blue-600  text-white ">
-            <Link to="/register">Register</Link>
-          </button>
-          <button className="mt-2 h-12 text-center px-6 py-2 shadow-lg rounded-md border text-blue-500 border-gray-200 ">
-            <Link to="/login">Login</Link>
-          </button>
+          <Link
+            className="mt-2  text-center px-6 py-2 shadow-lg rounded-md bg-blue-600 text-white"
+            to="/register"
+          >
+            Register
+          </Link>
+          <Link
+            className="mt-2  text-center px-6 py-2 shadow-lg rounded-md border text-blue-500 border-gray-200 "
+            to="/login"
+          >
+            Login
+          </Link>
         </div>
       </form>
     </div>
