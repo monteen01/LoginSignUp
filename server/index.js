cb65b944ebc0f0ca31feaaaa87889e29f9262b17
monteen <montwork01@gmail.com>
2023-11-28 12:14:36 +0530

    changes the path

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ dotenv.config();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://main--graceful-liger-e1f61c.netlify.app"],
+    origin: ["https://react-loginform.onrender.com"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
